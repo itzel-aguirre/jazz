989762df0c32fb68b7247b61504b770cc5fe1c7a
Amilkhael <amilkhael@outlook.com>
2019-03-24 18:32:30 -0600

    Creación del slider principal
    1. Agregué la librería de Owl carousel para generar el slider de cards.
    2. Hice los estilos para las cards junto con su iconografía.
    3. Adaptar el diseño a los dieferentes dispositivos.
    4. Reacomodé las ojas de estilos en sass.
    5. El tamaño de las imágenes para móviles es de 300 x 280 px y para desktop 450 x 400px
    6. Agregué un espacio para un copi pequeño.

--- footer.php
+++ footer.php
@@ -1,27 +1,59 @@
   <footer class="container-fluid footer">
-    <div class="p-2 d-flex justify-content-center">
-        <a href="https://www.facebook.com/parkerandlenox/" target="_blank">
-          <i class="mdi mdi-facebook mdi-24px icon"></i>
-        </a>
-        <a href="https://twitter.com/parkerandlenox" target="_blank">
-          <i class="mdi mdi-twitter mdi-24px icon"></i>
-        </a>
-        <a href="https://www.instagram.com/parkerandlenox/" target="_blank">
-          <i class="mdi mdi-instagram mdi-24px icon"></i>
-        </a>
-        <i class="mdi mdi-spotify mdi-24px icon"></i>
-    </div>
-    <div class="row">
-      <div class="col-6">
-        <a href="#"><p class="text-center link">Términos y Condiciones</p></a>
+      <div class="p-2 d-flex justify-content-center">
+          <a href="https://www.facebook.com/parkerandlenox/" target="_blank">
+              <i class="mdi mdi-facebook mdi-24px  icon icon--margin-right icon--color-gold"></i>
+          </a>
+          <a href="https://twitter.com/parkerandlenox" target="_blank">
+              <i class="mdi mdi-twitter mdi-24px icon icon--margin-right icon--color-gold"></i>
+          </a>
+          <a href="https://www.instagram.com/parkerandlenox/" target="_blank">
+              <i class="mdi mdi-instagram mdi-24px icon icon--margin-right icon--color-gold"></i>
+          </a>
+          <i class="mdi mdi-spotify mdi-24px icon icon--margin-right icon--color-gold"></i>
       </div>
-      <div class="col-6">
-        <a href="#"><p class="text-center link">Política de Privacidad</p></a>
+      <div class="row">
+          <div class="col-6">
+              <a href="#">
+                  <p class="text-center link">Términos y Condiciones</p>
+              </a>
+          </div>
+          <div class="col-6">
+              <a href="#">
+                  <p class="text-center link">Política de Privacidad</p>
+              </a>
+          </div>
       </div>
-    </div>
   </footer>
   <!--js-->
-	<script src="js/jquery-3.1.1.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-</body>
-</html>
+  <script src="http://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+  <!-- Carousel -->
+  <script type="text/javascript">
+      $(document).ready(function() {
+          $(".owl-carousel").owlCarousel({
+              center: true,
+              autoplay: true,
+              autoPlayTimeout: 5000,
+              autoplayHoverPause: true,
+              loop: true,
+              margin: 10,
+              dots:false,
+              responsive: {
+                  0: {
+                      items: 1
+                  },
+                  768: {
+                      items: 3
+                  },
+                  1000: {
+                      items: 4
+                  }
+              }
+          });
+      });
+  </script>
+  <!-- End Carousel -->
+  </body>
+
+  </html> 
--- index.php
+++ index.php
@@ -2,8 +2,160 @@
 include 'header.php';
 /**
 * 	@author Amilkhael Chávez Delgado;
-*	Documento: Index del micrositio
+*	Documento: Index del la landing
 */
 ?>
 
-<?php include 'footer.php';?>
+<!--Carousel Wrapper-->
+<div class="container-fluid">
+    <div class="row">
+        <div class="main-slider owl-carousel">
+            <div class="item">
+                <div class="card">
+                    <picture>
+                        <source media="(min-width: 1200px)" srcset="https://placeimg.com/450/400/animals">
+                        <img class="card-img-top" src="images/slider/mobile/abelMirales.jpg" alt="Card image cap">
+                    </picture>
+                    <div class="card-body">
+                        <div class="artist-info">
+                            <p class="performer"><i class="mdi mdi-account-outline icon--margin-right"></i>Abel Mireles Cuarteto</p>
+                            <p class="genre"><i class="mdi mdi-music icon--margin-right"></i>Jazz Contemporáneo</p>
+                        </div>
+                        <div class="event-info">
+                            <div class="row no-gutters">
+                                <div class="col-6">
+                                    <p class="date"><i class="mdi mdi-calendar-text icon--margin-right"></i>26 mar</p>
+                                    <p class="time"><i class="mdi mdi-clock-outline icon--margin-right"></i>22:30 h</p>
+                                    <p class="cover"><i class="mdi mdi-cash icon--margin-right"></i>No cover</p>
+                                </div>
+                                <div class="col-6 align-self-center">
+                                    <div class="row justify-content-center">
+                                        <button type="button" class="btn btn-primary btn-lg"><i class="mdi mdi-notebook icon--margin-right"></i>Reserva</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="card">
+                    <picture>
+                        <source media="(min-width: 1200px)" srcset="https://placeimg.com/450/400/nature">
+                        <img class="card-img-top" src="images/slider/mobile/amy.jpg" alt="Card image cap">
+                    </picture>
+                    <div class="card-body">
+                        <div class="artist-info">
+                            <p class="performer"><i class="mdi mdi-account-outline icon--margin-right"></i>Homenaje a Amy Winehouse por Xiomara Méndez</p>
+                            <p class="genre"><i class="mdi mdi-music icon--margin-right"></i>Jazz / R&B / Soul</p>
+                        </div>
+                        <div class="event-info">
+                            <div class="row no-gutters">
+                                <div class="col-6">
+                                    <p class="date"><i class="mdi mdi-calendar-text icon--margin-right"></i>03 abr</p>
+                                    <p class="time"><i class="mdi mdi-clock-outline icon--margin-right"></i>22:30 h</p>
+                                    <p class="cover"><i class="mdi mdi-cash icon--margin-right"></i>$150.00</p>
+                                </div>
+                                <div class="col-6 align-self-center">
+                                    <div class="row justify-content-center">
+                                        <button type="button" class="btn btn-primary btn-lg"><i class="mdi mdi-notebook icon--margin-right"></i>Reserva</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="card">
+                    <picture>
+                        <source media="(min-width: 1200px)" srcset="https://placeimg.com/450/400/any">
+                        <img class="card-img-top" src="images/slider/mobile/testCrosby.jpg" alt="Card image cap">
+                    </picture>
+                    <div class="card-body">
+                        <div class="artist-info">
+                            <p class="performer"><i class="mdi mdi-account-outline icon--margin-right"></i>Ted Crosby Quartet</p>
+                            <p class="genre"><i class="mdi mdi-music icon--margin-right"></i>Jazz Contemporáneo</p>
+                        </div>
+                        <div class="event-info">
+                            <div class="row no-gutters">
+                                <div class="col-6">
+                                    <p class="date"><i class="mdi mdi-calendar-text icon--margin-right"></i>28 mar</p>
+                                    <p class="time"><i class="mdi mdi-clock-outline icon--margin-right"></i>22:00 h</p>
+                                    <p class="cover"><i class="mdi mdi-cash icon--margin-right"></i>No cover</p>
+                                </div>
+                                <div class="col-6 align-self-center">
+                                    <div class="row justify-content-center">
+                                        <button type="button" class="btn btn-primary btn-lg"><i class="mdi mdi-notebook icon--margin-right"></i>Reserva</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="card">
+                    <picture>
+                        <source media="(min-width: 1200px)" srcset="https://placeimg.com/450/400/people">
+                        <img class="card-img-top" src="images/slider/mobile/afrobeat.jpg" alt="Card image cap">
+                    </picture>
+                    <div class="card-body">
+                        <div class="artist-info">
+                            <p class="performer"><i class="mdi mdi-account-outline icon--margin-right"></i>Afrobeat</p>
+                            <p class="genre"><i class="mdi mdi-music icon--margin-right"></i>World Music</p>
+                        </div>
+                        <div class="event-info">
+                            <div class="row no-gutters">
+                                <div class="col-6">
+                                    <p class="date"><i class="mdi mdi-calendar-text icon--margin-right"></i>30 mar</p>
+                                    <p class="time"><i class="mdi mdi-clock-outline icon--margin-right"></i>22:30 h</p>
+                                    <p class="cover"><i class="mdi mdi-cash icon--margin-right"></i>$150.00</p>
+                                </div>
+                                <div class="col-6 align-self-center">
+                                    <div class="row justify-content-center">
+                                        <button type="button" class="btn btn-primary btn-lg"><i class="mdi mdi-notebook icon--margin-right"></i>Reserva</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <div class="card">
+                    <picture>
+                        <source media="(min-width: 1200px)" srcset="https://placeimg.com/450/400/tech/sepia">
+                        <img class="card-img-top" src="images/slider/mobile/NiteJewel.jpg" alt="Card image cap">
+                    </picture>
+                    <div class="card-body">
+                        <div class="artist-info">
+                            <p class="performer"><i class="mdi mdi-account-outline icon--margin-right"></i>Nite Jewel (LA)</p>
+                            <p class="genre"><i class="mdi mdi-music icon--margin-right"></i>R&B / Electrónica / POP</p>
+                        </div>
+                        <div class="event-info">
+                            <div class="row no-gutters">
+                                <div class="col-6">
+                                    <p class="date"><i class="mdi mdi-calendar-text icon--margin-right"></i>27 mar</p>
+                                    <p class="time"><i class="mdi mdi-clock-outline icon--margin-right"></i>22:00 h</p>
+                                    <p class="cover"><i class="mdi mdi-cash icon--margin-right"></i>$150.00</p>
+                                </div>
+                                <div class="col-6 align-self-center">
+                                    <div class="row justify-content-center">
+                                        <button type="button" class="btn btn-primary btn-lg"><i class="mdi mdi-notebook icon--margin-right"></i>Reserva</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row row--background-blue">
+        <div class="col">
+          <p class="first-copy">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ac purus eget lectus pharetra consequat.</p>
+        </div>
+      </div>
+</div>
+<?php include 'footer.php'; ?> 
