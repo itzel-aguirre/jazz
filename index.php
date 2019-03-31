0ea7ac643064000a7ea83a72df5b06a6a018f2be
Amilkhael <amilkhael@outlook.com>
2019-03-31 16:24:57 -0600

    Formulario de reservacion
    1. Se creó el formulario para eralizar una resrevación
    2. Se hicieron los estilos para los inputs y los select
    3. Se adaptó el estilo a los diferentes dispositivos

--- index.php
+++ index.php
@@ -233,6 +233,70 @@ include 'header.php';
         </div>
     </div>
     <div class="row">
+        <div class="col-12">
+            <form action="">
+                <div class="form-row justify-content-center">
+                    <fieldset class="col-12 col-md-6 col-xl-5">
+                        <legend>Información Personal</legend>
+
+                        <div class="form-group">
+                            <label for="name">Nombre completo</label>
+                            <input id="name" type="text" class="form-control input-text" placeholder="Nombre">
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Correo electrónico</label>
+                            <input type="email" id="email" class="form-control input-text" placeholder="correo@ejemplo.com">
+                        </div>
+                        <div class="form-group">
+                            <label for="mobile">Celular</label>
+                            <input type="number" id="mobile" class="form-control input-text" placeholder="5540123487">
+                        </div>
+                    </fieldset>
+                    <fieldset  class="col-12 col-md-6 col-xl-5">
+                        <legend>Información del Evento</legend>
+                        <div class="form-group">
+                            <label for="show">Espectáculo</label>
+                            <select id="show" class="form-control" required>
+                                <option value="" disabled selected>Selecciona</option>
+                                <option value="volvo">Ted Crosby</option>
+                                <option value="saab">Homenaje a Amy Winehouse por Xiomara Méndez</option>
+                                <option value="mercedes">Afrobeat</option>
+                                <option value="audi">Nite Jewel</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="date-time">Fecha y hora</label>
+                            <select id="date-time" class="form-control " required>
+                                <option value="" disabled selected>Selecciona</option>
+                                <option value="volvo">26 mar / 22:30</option>
+                            </select>
+                        </div>
+                        <div class="row ">
+                            <div class="form-group col-6">
+                                <label for="clients"># Personas</label>
+                                <input type="number" id="clients" class="form-control input-text" min="1" max="30" value="1">
+                            </div>
+                            <div class="form-group col-6 align-self-end">
+                                <label for="table">Mesa</label>
+                                <select id="table" class="form-control" required>
+                                    <option value="" disabled selected>Selecciona</option>
+                                    <option value="volvo">1</option>
+                                    <option value="volvo">50</option>
+                                    <option value="volvo">23</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="row justify-content-end">
+                                <button class="btn btn-primary btn-lg">Enviar</button>
+                            </div>
+                        </div>
+                    </fieldset>
+                </div>
+            </form>
+        </div>
+    </div>
+    <div class="row">
         <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d517.8516407387972!2d-99.15644784371455!3d19.43061077713533!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x232dad92e8d474db!2sParker+%26+Lenox!5e0!3m2!1sen!2smx!4v1553575383953" class="map" frameborder="0" style="border:0" allowfullscreen></iframe>
     </div>
 </div>
