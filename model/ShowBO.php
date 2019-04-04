ce72e6f504107bb720fbd5bae4b45cf66410ea52
Amilkhael <amilkhael@outlook.com>
2019-04-03 20:27:20 -0600

    Base para back

---
+++ model/ShowBO.php
@@ -0,0 +1,26 @@
+<?php
+include_once 'ConectaBD.php';
+include_once 'Show.php';
+class ShowBO
+{
+  //Atributos
+  private $post;
+
+  //Constructor
+  public function __construct($post)
+  {
+    $this->post=$post;
+  }
+
+  //Methods
+  public function createShow($showData){
+
+  }
+  public function deleteShow($idShow){
+
+  }
+  public function updateShow($showData){
+    
+  }
+}
+?>
