d2d4fa0981bc8da5c9e6eae694c60d57ee137446
Amilkhael <amilkhael@outlook.com>
2019-04-06 20:01:24 -0600

    Add model clases and create user Clases

--- model/ShowBO.php
+++ model/ShowBO.php
@@ -14,23 +14,7 @@ class ShowBO
 
   //Methods
   public function createShow($showData){
-    $DBConestarBd = new ConectDB;
-    $DBConestarBd->conectar();
-    $query = "SELECT usuarios.USUARIO, usuarios.ROL FROM `usuarios` WHERE usuarios.CORREO = '' AND usuarios.CONTRASEÑA= ''";
-    $respuesta=$DBConestarBd->consulta($query);
-    $DBConestarBd->desconectar();
-    $row_cnt = $respuesta->num_rows;
-    if($row_cnt>0)
-    {
-    $info = $resultrespuestaado->fetch_fields();
-    $row = mysqli_fetch_array($info, MYSQLI_ASSOC);
- 
-    return  $row["USUARIO"].$row["ROL"];
-    }
-    else{
-    return "No existe usuario"; 
-    }
-
+  
   }
 
   public function deleteShow($idShow){
