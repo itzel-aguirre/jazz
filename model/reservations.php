b766a1a2cbade77aed5f290a3d7c076996bad537
itzel-aguirre <48694003+itzel-aguirre@users.noreply.github.com>
2019-04-12 17:30:57 -0500

    Cambios en funciones

--- model/reservations.php
+++ model/reservations.php
@@ -1,22 +1,42 @@
 <?php
-class reservationsBO
+class reservations
 {
  //Attributes
   private $id_reservation;
   private $id_show;
-  private $id_mesa;
+  private $id_table;
   private $id_date_hr;
   private $full_name;
   private $mail;
   private $cell_phone;
   private $deposit_made;
   private $no_people;
+  private $artist;
+  private $cover;
+  private $date;
+  private $hour;
+  private $no_table;
 
- //Constructor
- public function __construct()
- {
+  //Constructor
+  public function __construct()
+  {
+    
+  }
+  public static function constructPost( $dataReservation) {
+    $instance = new self();
+    $instance->id_reservation = $dataReservation->id_reservation;
 
- }
+    $instance->id_show = $dataReservation->id_show;
+    $instance->id_table =  $dataReservation->id_table;
+    $instance->id_date_hr =  $dataReservation->id_date_hr;
+    $instance->full_name= $dataReservation->full_name;
+    $instance->mail =  $dataReservation->mail;
+    $instance->cell_phone =  $dataReservation->cell_phone;
+    $instance->deposit_made = $dataReservation->deposit_made;
+    $instance->no_people = $dataReservation->no_people;
+   
+    return $instance; 
+  }
 //Methods
   // Getter/Setter not defined so set as property of object
   public function __set($name,$value){
--- model/reservationsBO.php
+++ model/reservationsBO.php
@@ -1,20 +1,11 @@
 <?php
-include_once 'ConectaBD.php';
+include_once 'ConectDB.php';
 include_once 'reservations.php';
-
 
 class reservationsBO
 {
-    //Atributos
-  private $post;
-
-  //Constructor
-  public function __construct($post)
-  {
-    $this->post = $post;
-  }
-
-  public function Reservation($reservationDData)
+ 
+  public function Reservation($ReservationData)
   {
     $databaseConected = new ConectDB();
     $databaseConected->conectar();
@@ -23,20 +14,35 @@ class reservationsBO
     $query .= ", reservaciones.NOMBRE_COMPLETO, reservaciones.CORREO, reservaciones.CELULAR, reservaciones.DEPOSITO_REALIZADO, reservaciones.NO_PERSONAS";
     $query .= ", espectaculos.ARTISTA, espectaculos.COVER, fecha_hr_espectaculo.FECHA, fecha_hr_espectaculo.HORA";
     $query .= ", mesas.NO_MESA";
-    $query .= "FROM `reservaciones`";
-    $query .= "INNER JOIN `espectaculos` ON reservaciones.ID_ESPECTACULO= espectaculos.ID_ESPECTACULO";
-    $query .= "INNER JOIN `fecha_hr_espectaculo` ON reservaciones.ID_FECHA_HR = fecha_hr_espectaculo.ID_FECHA_HR";
-    $query .= "INNER JOIN `mesas` ON reservaciones.ID_MESA = mesas.ID_MESA";
+    $query .= " FROM `reservaciones` ";
+    $query .= " INNER JOIN `espectaculos` ON `reservaciones`.`ID_ESPECTACULO` = `espectaculos`.`ID_ESPECTACULO`";
+    $query .= " INNER JOIN `fecha_hr_espectaculo` ON reservaciones.ID_FECHA_HR = fecha_hr_espectaculo.ID_FECHA_HR";
+    $query .= " INNER JOIN `mesas` ON reservaciones.ID_MESA = mesas.ID_MESA";
+    //$query .= " WHERE reservaciones.ID_RESERVACION= '".$ReservationData->id_reservation."'";
     $Reservation = $databaseConected->consulta($query);
-   
+    $databaseConected->desconectar();
+
     if ($Reservation->num_rows > 0) {
-        $info = $Reservation->fetch_fields();
-        $row = mysqli_fetch_array($info, MYSQLI_ASSOC);
-        $databaseConected->desconectar();
-        return $info;
+        $dataUserObj = new reservations();
+        $row = $Reservation->fetch_assoc(); 
+        $dataUserObj->id_reservation = $row["ID_RESERVACION"];
+        $dataUserObj->id_show = $row["ID_ESPECTACULO"];
+        $dataUserObj->id_table = $row["ID_MESA"];
+        $dataUserObj->id_date_hr = $row["ID_FECHA_HR"];
+        $dataUserObj->full_name = $row["NOMBRE_COMPLETO"];
+        $dataUserObj->mail = $row["CORREO"];
+        $dataUserObj->cell_phone = $row["CELULAR"];
+        $dataUserObj->deposit_made = $row["DEPOSITO_REALIZADO"];
+        $dataUserObj->no_people = $row["NO_PERSONAS"];
+        $dataUserObj->artist = $row["ARTISTA"];
+        $dataUserObj->cover = $row["COVER"];
+        $dataUserObj->date = $row["FECHA"];
+        $dataUserObj->hour = $row["HORA"];
+        $dataUserObj->no_table = $row["NO_MESA"];
+        
+        return $dataUserObj;
       } else {
-        $databaseConected->desconectar();
-      return json_encode (array('error'=>'No hay datos a mostrar'));
+        return json_encode (array('error'=>"No hay reservaciones"));
     }
    
   }
@@ -45,54 +51,48 @@ class reservationsBO
   {  $databaseConected = new ConectDB();
     $databaseConected->conectar();
 
-    $query = "DELETE FROM `reservaciones` WHERE `id_reservacion`=".$idReservation."";
-    $databaseConected->consulta($query);
-
-    if ($databaseConected->query($query) == FALSE) {
-        $databaseConected->desconectar();
-        return "Genero Eliminado";
+    $query = "DELETE FROM `reservaciones` WHERE reservaciones.ID_RESERVACION=".$idReservation->id_reservation.";";
+    $ReservationInfo =$databaseConected->consulta($query);
+    $databaseConected->desconectar();
+    if ($ReservationInfo) {
+      return json_encode(TRUE);  
     }
     else{
-        $databaseConected->desconectar();
-        return "Error: " . mysqli_error($databaseConected);
+      return json_encode (array('error'=>"Error: " . mysqli_error($databaseConected)));
     }
     
-}
+  }
 
   public function UpdateReservation($ReservationData)
   {  $databaseConected = new ConectDB();
     $databaseConected->conectar();
-    $query ="UPDATE `reservaciones` SET ,`ID_ESPECTACULO`=".$ReservationData->id_show.", `ID_MESA`=".$ReservationData->id_mesa.", `ID_FECHA_HR`=".$ReservationData->id_date_hr.", ";
-    $query .="`NOMBRE_COMPLETO`='".$ReservationData->full_name."', `CORREO`='".$ReservationData->email."', `CELULAR`=".$ReservationData->cell_phone.", `DEPOSITO_REALIZADO`=".$ReservationData->deposit_made.", `NO_PERSONAS`=".$ReservationData->no_people." ";
-    $query .="WHERE `ID_RESERVACION`=".$ReservationData->id_reservation." ";
+    $query ="UPDATE `reservaciones` SET `ID_ESPECTACULO`=".$ReservationData->id_show.", `ID_MESA`=".$ReservationData->id_table.", `ID_FECHA_HR`=".$ReservationData->id_date_hr.", ";
+    $query .="`NOMBRE_COMPLETO`='".$ReservationData->full_name."', `CORREO`='".$ReservationData->mail."', `CELULAR`=".$ReservationData->cell_phone.", `DEPOSITO_REALIZADO`=".$ReservationData->deposit_made.", `NO_PERSONAS`=".$ReservationData->no_people." ";
+    $query .="WHERE `ID_RESERVACION`=".$ReservationData->id_reservation.";";
     $ReservationInfo = $databaseConected->consulta($query);
-    
-    if ($databaseConected->query($query) === TRUE) {
-        $databaseConected->desconectar();
-        return true; //"Reservación actualizada";
+    $databaseConected->desconectar();
+    if ($ReservationInfo) {
+      return json_encode(TRUE);  
     }
     else{
-        $databaseConected->desconectar();
-        return "Error: " . mysqli_error($databaseConected);
-   }
- 
-}
+      return json_encode (array('error'=>"Error: " . mysqli_error($databaseConected)));
+    }
+  }
 
   public function CreateReservation($ReservationData)
   {  $databaseConected = new ConectDB();
     $databaseConected->conectar();
-    $query ="INSERT INTO `RESERVACIONES` (`ID_ESPECTACULO`, `ID_FECHA_HR`, ";
-    $query .="`ID_MESA`, `NOMBRE_COMPLETO`, `CORREO`, `CELULAR`, `DEPOSITO_REALIZADO`) VALUES ";
-    $query .="(".$ReservationData->id_show.", ".$ReservationData->id_date_hr.", ".$ReservationData->id_mesa.", '".$ReservationData->full_name."', '".$ReservationData->email."', ".$ReservationData->cell_phone.", '0')";
+    $query ="INSERT INTO `RESERVACIONES` "; 
+    $query .="(`ID_RESERVACION`, `ID_ESPECTACULO`, `ID_MESA`, `ID_FECHA_HR`, `NOMBRE_COMPLETO`, `CORREO`, `CELULAR`, `DEPOSITO_REALIZADO`, `NO_PERSONAS`) VALUES ";
+    $query .="('', ".$ReservationData->id_show.", ".$ReservationData->id_table.", ".$ReservationData->id_date_hr.", '".$ReservationData->full_name."', '".$ReservationData->mail."', ".$ReservationData->cell_phone.", '0', ".$ReservationData->no_people.")";
     $ReservationInfo = $databaseConected->consulta($query);
-    if ($ReservationInfo->num_rows > 0) {
-      
-        $databaseConected->desconectar();
-        return json_encode (array('error'=>'Rerervación registrada'));
-      } else {
-        $databaseConected->desconectar();
-      return json_encode (array('error'=>'Error al registrar la reservación'));
+    $databaseConected->desconectar();
+    if ($ReservationInfo) {
+        return json_encode(TRUE);  
     }
+    else{
+        return json_encode (array('error'=>"Error: " . mysqli_error($databaseConected)));
     }
+  }
 }
 ?>
