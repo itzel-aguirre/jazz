451e644753b684daa4d7f48d881a1f857b030c25
Amilkhael <amilkhael@outlook.com>
2019-04-21 01:53:51 -0500

    Validación de formularios
    1. Se cambió el método para validar campos requeridos. Utilizando ES6 en lugar de JQUERY

--- js/utils.js
+++ js/utils.js
@@ -1,168 +1,157 @@
-function validateEmail(email,idInput) {
+function validateEmail(email, idInput) {
   const emailRegex = /(.+.*@.+.*\..+.*)/;
-  if (email.match(emailRegex)){
+  if (email.match(emailRegex)) {
     $(idInput).removeClass("input-text--error");
     return true;
-  } else{
+  } else {
     $(idInput)
-    .siblings(".error")
-    .show();
+      .siblings(".error")
+      .show();
     $(idInput)
-    .siblings(".error")
-    .text("Ingresa un correo electrónico válido");
+      .siblings(".error")
+      .text("Ingresa un correo electrónico válido");
     $(idInput).addClass("input-text--error");
     return false;
   }
-  
 }
 
 function validateRequiredFileds(form) {
-  let isValid = false;
-  $(form)
-    .find("input")
-    .each(function() {
-      if ($(this).prop("required") && $(this).val().length === 0) {
-        $(this)
-          .siblings(".error")
-          .text("Campo requerido");
-        $(this)
-          .siblings(".error")
-          .show();
-        $(this).addClass("input-text--error");
-        isValid = false;
-      } else {
-        $(this)
-          .siblings(".error")
-          .hide();
-        isValid = true;
-        $(this).removeClass("input-text--error");
-      }
-    });
+  const requiredFields = Array.from(
+    document.querySelectorAll(`${form} *:required`)
+  ).filter(field => {
+    const simblings = Array.from(field.parentNode.children);
+    if (!field.value) {
+      simblings.find(simbling => {
+        if (simbling.className === "error") {
+          simbling.innerText = "Campo Requerido";
+        }
 
-    $(form)
-    .find("select")
-    .each(function() {
-     
-      if ($(this).prop("required")) {
-        $(this)
-          .siblings(".error")
-          .text("Campo requerido");
-        $(this)
-          .siblings(".error")
-          .show();
-        $(this).addClass("select-text--error");
-        isValid = false;
-      } else {
-        $(this)
-          .siblings(".error")
-          .hide();
-        isValid = true;
-        $(this).removeClass("select-text--error");
-      }
-    });
-  return isValid;
+        if (
+          simbling.localName !== "label" &&
+          !simbling.className.includes("--error") &&
+          simbling.localName !== "p"
+        ) {
+          const modifier = simbling.classList[1] + "--error";
+          simbling.className += ` ${modifier}`;
+        }
+      });
+    } else {
+      simblings.find(simbling => {
+        if (simbling.className === "error") simbling.innerText = "";
+        if (simbling.localName !== "label") {
+          simbling.classList = Array.from(simbling.classList)
+            .filter(classN => !classN.includes("--error"))
+            .join(" ");
+        }
+      });
+    }
+    return field.value === "";
+  });
+  return requiredFields.length > 0 ? false : true;
 }
 
-function validateArray(array,element,errorText){
-  if(array.length>0){
-    $(element).siblings(".error")
-    .hide();
-    return true
-  }
-  else{
+function validateArray(array, element, errorText) {
+  if (array.length > 0) {
     $(element)
-        .siblings(".error")
-        .show();
-      $(element)
-        .siblings(".error")
-        .text(errorText);
-        return false;
+      .siblings(".error")
+      .hide();
+    return true;
+  } else {
+    $(element)
+      .siblings(".error")
+      .show();
+    $(element)
+      .siblings(".error")
+      .text(errorText);
+    return false;
   }
 }
 
-function notifications (text, type){
-  let {information,currentClass} = ""
+function notifications(text, type) {
+  let { information, currentClass } = "";
 
   switch (type) {
-    case 'success':
-        information= '<strong>¡Éxito!</strong> '
-        $('.alert').addClass( "alert-success" )
-        currentClass = "alert-success"
-        break;
-    case 'info':
-        information= '<strong>¡Información!</strong> '
-        $('.alert').addClass( "alert-info" );
-        currentClass = "alert-info"
-        break;
-    case 'warning':
-        information= '<strong>¡Advetrencia!</strong> '
-        $('.alert').addClass( "alert-warning" );
-        currentClass = "alert-warning"
-        break;
-    case 'error':
-        information= '<strong>¡Érror!</strong> '
-        $('.alert').addClass( "alert-danger" );
-        currentClass = "alert-danger"
-        break;
-}
-  information+=text;
-  
-  $('.alert p.information').html(information)
-  $('.alert').fadeIn('slow');
-  
-  $(".alert").delay(3000).fadeOut("slow", function() {
-    $(this).removeClass(currentClass);
-  });
+    case "success":
+      information = "<strong>¡Éxito!</strong> ";
+      $(".alert").addClass("alert-success");
+      currentClass = "alert-success";
+      break;
+    case "info":
+      information = "<strong>¡Información!</strong> ";
+      $(".alert").addClass("alert-info");
+      currentClass = "alert-info";
+      break;
+    case "warning":
+      information = "<strong>¡Advetrencia!</strong> ";
+      $(".alert").addClass("alert-warning");
+      currentClass = "alert-warning";
+      break;
+    case "error":
+      information = "<strong>¡Érror!</strong> ";
+      $(".alert").addClass("alert-danger");
+      currentClass = "alert-danger";
+      break;
+  }
+  information += text;
 
+  $(".alert p.information").html(information);
+  $(".alert").fadeIn("slow");
+
+  $(".alert")
+    .delay(3000)
+    .fadeOut("slow", function() {
+      $(this).removeClass(currentClass);
+    });
 }
 
-function validarReservation(){
+function validarReservation() {
   if (validateRequiredFileds("#reserveForm")) {
+    if (validateEmail($("#email").val())) {
+      $("#email").removeClass("input-text--error");
+      $("#name").removeClass("input-text--error");
+      $("#mobile").removeClass("input-text--error");
 
-      if (validateEmail($("#email").val())) {
-    
-              $("#email").removeClass("input-text--error")
-              $("#name").removeClass("input-text--error")
-              $("#mobile").removeClass("input-text--error")
+      const email = $("#email").val();
+      const name = $("#name").val();
+      const mobile = $("#mobile").val();
+      const show = $("#show").val();
+      const dateTime = $("#date-time").val();
+      const clients = $("#clients").val();
+      const table = $("#table").val();
 
-              const email = $("#email").val();
-              const name = $("#name").val();
-              const mobile = $("#mobile").val();
-              const show =$("#show").val();
-              const dateTime =$("#date-time").val();
-              const clients =$("#clients").val();
-              const table =$("#table").val();
-
-              let loginData = {
-              "email": email,
-              "name": name,
-              "mobile": mobile,
-              "show": show,
-              "dateTime": dateTime,
-              "clients": clients,
-              "table": table
-             }
-              $.ajax({
-              type: "POST",
-              url: "controller/controller-CreateReservation.php",
-              data: JSON.stringify(loginData),
-              contentType: "application/json; charset=utf-8",
-              dataType: "json",
-              success: function(data) {
-              //$("#login-form").text("Reservación registrada")
-              },
-              error: function(errMsg) {
-               console.error(errMsg);
-              $("#reserveForm").siblings(".error").text(errMsg)
-              }
-          });
-          }
-          else {
-           $("#email").siblings(".error").show()
-           $("#email").siblings(".error").text("Ingresa un correo electrónico válido")
-           $("#email").addClass("input-text--error")
+      let loginData = {
+        email: email,
+        name: name,
+        mobile: mobile,
+        show: show,
+        dateTime: dateTime,
+        clients: clients,
+        table: table
+      };
+      $.ajax({
+        type: "POST",
+        url: "controller/controller-CreateReservation.php",
+        data: JSON.stringify(loginData),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function(data) {
+          //$("#login-form").text("Reservación registrada")
+        },
+        error: function(errMsg) {
+          console.error(errMsg);
+          $("#reserveForm")
+            .siblings(".error")
+            .text(errMsg);
         }
+      });
+    } else {
+      $("#email")
+        .siblings(".error")
+        .show();
+      $("#email")
+        .siblings(".error")
+        .text("Ingresa un correo electrónico válido");
+      $("#email").addClass("input-text--error");
+    }
   }
 }
-
-
